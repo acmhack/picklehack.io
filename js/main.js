71f47fb64ee3cb87a08ff5fb6f102f4af550de7f
Dylan Burr <djbn65@umsystem.edu>
2020-09-22 20:12:54 -0500

    Add Firebase Integration

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+var databaseRef = firebase.database().ref('emails/');
 
-const octokit = new Octokit({ auth: '900d98da2d47e6b264950f9e45b79d4db6d27b77'});
+function checkAndStoreEmail() {
+    const email = $.trim($('.input input').val());
+    const emailValid = (-1 !== email.search(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/));
+    const emailInput = $('.input input');
 
-function emailValid(email) {
-    return -1 !== email.search(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if(emailValid) {
+        emailInput.css('border', '');
+        databaseRef.update({
+            [email.replace('.', ',')]: true
+        });
+    } else {
+        emailInput.addClass('invalid');
+    }
 }
 
-$('.input .subscribe').click(async () => {
-    const email = $('.input input').val();
+$('.input input').keyup(e => {
+    $('.input input').removeClass('invalid');
 
-    if(emailValid(email)) {
-        await octokit
+    if(e.which !== 13) {
+        return;
     }
+
+    checkAndStoreEmail();
 });
+
+$('.input .subscribe').click(checkAndStoreEmail);
