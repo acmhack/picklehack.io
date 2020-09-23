89e527184bf9c8ef7d82cda28187c006b0d5f84d
Dylan Burr <djbn65@umsystem.edu>
2020-09-23 13:06:24 -0500

    Add paralx moevement

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 var databaseRef = firebase.database().ref('emails/');
+var pickleImg = $('.pickle-img img');
 
 function checkAndStoreEmail() {
-    const email = $.trim($('.input input').val());
-    const emailValid = (-1 !== email.search(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/));
     const emailInput = $('.input input');
+    const email = $.trim(emailInput.val());
+    const emailValid = (-1 !== email.search(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/));
 
     if(emailValid) {
         emailInput.css('border', '');
         databaseRef.update({
             [email.replace('.', ',')]: true
         });
+
+        $('.modal-window').addClass('shown');
+        $('.modal').addClass('shown');
+        emailInput.val('');
+
+        setTimeout(() => {
+            $('.modal-window').removeClass('shown');
+            $('.modal').removeClass('shown');
+        }, 4000);
     } else {
         emailInput.addClass('invalid');
     }
@@ -26,3 +36,17 @@ $('.input input').keyup(e => {
 });
 
 $('.input .subscribe').click(checkAndStoreEmail);
+
+function calcLeft(x, maxLeft) {
+    return (-(maxLeft * 2) / $(window).width()) * x + maxLeft;
+}
+
+function calcTop(y, maxTop) {
+    return (-(maxTop * 2) / $(window).innerHeight()) * y + maxTop;
+}
+
+$(document).mousemove((e) => {
+    const maxLeft = $(window).width() / 64;
+    const maxTop = $(window).innerHeight() / 46.85
+    pickleImg.css('left', calcLeft(e.pageX, maxLeft)).css('top', calcTop(e.pageY, maxTop));
+});
